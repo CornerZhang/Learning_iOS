02bfa754f1b191ba6b904bf9c057ecd75616b649
CornerZhang <cyberzei@qq.com>
2014-08-26 23:07:39 +0800

    ＊ TableViewToDetails

---
+++ Interface/TableViewToDetails/TableViewToDetails/NXTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  NXTableViewController.h
+//  TableViewToDetails
+//
+//  Created by CornerZhang on 14-8-26.
+//  Copyright (c) 2014年 NeXtreme.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NXTableViewController : UITableViewController
+
+@end
