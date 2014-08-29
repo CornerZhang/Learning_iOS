5779153791ce3e3a3b6535bdaa05d4df4fc062e0
CornerZhang <cyberzei@qq.com>
2014-08-29 12:14:13 +0800

    * some modify

---
+++ Functions/CalendarEventReminder/CalendarEventReminder/NXViewController.h
@@ -0,0 +1,13 @@
+//
+//  NXViewController.h
+//  CalendarEventReminder
+//
+//  Created by CornerZhang on 14-8-15.
+//  Copyright (c) 2014年 NeXtreme.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NXViewController : UIViewController
+
+@end
---
+++ Functions/CalendarEventReminder/CalendarEventReminder/NXViewController.m
@@ -0,0 +1,29 @@
+//
+//  NXViewController.m
+//  CalendarEventReminder
+//
+//  Created by CornerZhang on 14-8-15.
+//  Copyright (c) 2014年 NeXtreme.com. All rights reserved.
+//
+
+#import "NXViewController.h"
+
+@interface NXViewController ()
+
+@end
+
+@implementation NXViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
